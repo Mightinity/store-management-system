d80c62fa6e29114d5704bf2d2ff99292cde0404e
Mightinity <yudistira.bimo1312@gmail.com>
2023-11-20 12:10:16 +0700

    Update productLayout and productController

--- src/main/java/com/systeminventory/controller/productController.java
+++ src/main/java/com/systeminventory/controller/productController.java
@@ -5,28 +5,117 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 
 import java.io.IOException;
 
 public class productController {
 
     @FXML
-    private Button dashboardButton;
+    private Pane profileDropdown;
 
     @FXML
-    private Button productButton;
+    private Button settingsDropdown;
 
     @FXML
-    private ScrollPane productPane;
+    private Button buttonCashier;
 
     @FXML
-    private void onClickDashboardButton(ActionEvent event) throws IOException {
+    private Button buttonProduct;
+
+    @FXML
+    private Button logoutDropdown;
+    @FXML
+    private Button buttonDashboard;
+    @FXML
+    private ScrollPane scrollPane;
+
+    @FXML
+    void onButtonCashierClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onButtonDashboardClick(ActionEvent event) throws IOException {
         App.loadDashboardScene();
     }
 
     @FXML
-    private void onClickProductButton(ActionEvent event) {
+    void onButtonProductClick(ActionEvent event) throws IOException {
+    }
+
+    @FXML
+    void onLogoutClick(ActionEvent event) throws IOException {
+        App.loadLogoutScene();
+    }
+
+    @FXML
+    void onProfileClick(MouseEvent event) {
+        if(profileDropdown.isVisible()){
+            profileDropdown.setVisible(false);
+        } else {
+            profileDropdown.setVisible(true);
+        }
+    }
+
+    @FXML
+    void onLogoutDropdownMouseEnter(MouseEvent event) {
+        logoutDropdown.setStyle("-fx-background-color: #2f3d4e;" + "-fx-background-radius: 11");
+    }
+
+    @FXML
+    void onLogoutDropdownMouseExit(MouseEvent event) {
+        logoutDropdown.setStyle("-fx-background-color: #1c242e;" + "-fx-background-radius: 11");
+    }
+
+    @FXML
+    void onSettingsDropdownMouseEnter(MouseEvent event) {
+        settingsDropdown.setStyle("-fx-background-color: #2f3d4e;" + "-fx-background-radius: 11");
+    }
+
+    @FXML
+    void onSettingsDropdownMouseExit(MouseEvent event) {
+        settingsDropdown.setStyle("-fx-background-color: #1c242e;" + "-fx-background-radius: 11");
+    }
+
+    @FXML
+    private void onButtonProductMouseEnter(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    private void onButtonProductMouseExit(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    private void onCashierProductMouseEnter(MouseEvent mouseEvent) {
+        buttonCashier.setStyle("-fx-background-color: #697b7b;" + "-fx-border-color: #697b7b;" + "-fx-text-fill: #151d26;" + "-fx-background-radius: 20;" + "-fx-border-radius: 20;");
+    }
+
+    @FXML
+    private void onCashierProductMouseExit(MouseEvent mouseEvent) {
+        buttonCashier.setStyle("-fx-background-color: #151d26;" + "-fx-border-color: #697b7b;" + "-fx-text-fill: #697b7b;" +  "-fx-background-radius: 20;" + "-fx-border-radius: 20;");
+    }
+
+    @FXML
+    private void onProfileDropdownMouseExit(MouseEvent mouseEvent) {
+        profileDropdown.setVisible(false);
+    }
+
+    @FXML
+    private void onButtonDashboardMouseEnter(MouseEvent mouseEvent) {
+        buttonDashboard.setStyle("-fx-background-color: #697b7b;" + "-fx-border-color: #697b7b;" + "-fx-text-fill: #151d26;" + "-fx-background-radius: 20;" + "-fx-border-radius: 20;");
 
     }
 
+    @FXML
+    private void onButtonDashboardMouseExit(MouseEvent mouseEvent) {
+        buttonDashboard.setStyle("-fx-background-color: #151d26;" + "-fx-border-color: #697b7b;" + "-fx-text-fill: #697b7b;" +  "-fx-background-radius: 20;" + "-fx-border-radius: 20;");
+    }
+
+    @FXML
+    private void initialize() {
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+    }
 }
